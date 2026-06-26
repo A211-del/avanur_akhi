119bdc1167cf99d74284dd6165c0ff1bd69aa26e
A211-del <akhiavanur@gmail.com>
2026-06-27 00:16:05 +0600

    Add files via upload

---
+++ celcius.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+    int c;
+    //celcius to fareinhite
+    c=31;
+    float equation;
+    float quation=(9*c)/5+32;
+    printf("%f", quation);
+    return 0;
+}
