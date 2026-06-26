0fac7af1612c69f5bfa67b2790528ce628d9c087
A211-del <akhiavanur@gmail.com>
2026-06-27 00:14:43 +0600

    Add files via upload

---
+++ hi.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+ int a=5;
+ int result;
+ // 'a' becomes 6 first, then 'result' gets the new value (6)
+ result= ++a;
+ printf("result: %d\n", result) ; //output 6
+ printf("value of a: %d\n", a);
+ //output: 6
+ return 0;
+
+}
